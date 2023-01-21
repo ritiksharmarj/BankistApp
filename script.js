3326656d8d0244af5beb6feb7ffd24bc7d3978ad
ritiksharmarj <bloggeritik@gmail.com>
2023-01-22 03:06:55 +0530

    Display summary

--- script.js
+++ script.js
@@ -36,31 +36,34 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
 const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+// const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector('.app');
+// const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
 
+/**
+ * Display deposit and withdrawal amount to the dashboard.
+ */
 const displayMovements = (movements) => {
    // "innerHTML" property replace HTML code with the new one.
    containerMovements.innerHTML = '';
@@ -73,7 +76,7 @@ const displayMovements = (movements) => {
             <div class="movements__type movements__type--${type}">${
          i + 1
       } ${type}</div>
-            <div class="movements__value">${movement}</div>
+            <div class="movements__value">${movement}€</div>
         </div>
       `;
 
@@ -82,6 +85,64 @@ const displayMovements = (movements) => {
    });
 };
 displayMovements(account1.movements);
+
+/**
+ * Display current balance to the dashboard.
+ */
+const calcDisplayBalance = (movements) => {
+   const balance = movements.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+   );
+
+   labelBalance.textContent = `${balance}€`;
+};
+calcDisplayBalance(account1.movements);
+
+/**
+ * Display total credit, debit and interest amount summary
+ */
+const calcDisplaySummary = (movements) => {
+   // Display incomes summary
+   const totalCreditAmount = movements
+      .filter((mov) => mov > 0)
+      .reduce((acc, mov) => acc + mov, 0);
+   labelSumIn.textContent = `${totalCreditAmount}€`;
+
+   // Display outcomes summary
+   const totalDebitAmount = movements
+      .filter((mov) => mov < 0)
+      .reduce((acc, mov) => acc + mov, 0);
+   labelSumOut.textContent = `${Math.abs(totalDebitAmount)}€`;
+
+   // Display total interest amount
+   const interest = movements
+      // filter out only deposit amount
+      .filter((mov) => mov > 0)
+      // calculate the interest for each deposit amount
+      .map((deposit) => (deposit * 1.2) / 100)
+      // bank's rule: interest amount should be greater than one to calculate in the total interest amount
+      .filter((interestAmount) => interestAmount >= 1)
+      // calc total interest amount
+      .reduce((acc, interestAmount) => acc + interestAmount, 0);
+   labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
+
+/**
+ * Create username using account's owner's name
+ * if name "Steven Thomas Williams" then username "stw"
+ */
+const createUserNames = (accs) => {
+   accs.forEach((acc) => {
+      acc.username = acc.owner
+         .toLowerCase()
+         .split(' ')
+         .map((name) => name[0])
+         .join('');
+   });
+};
+createUserNames(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
